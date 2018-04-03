8ea55c5b537e6e04e8eebd5ef485227f1901aed0
Egor Zviagintcev <ezvyagin@aquantia.com>
2018-04-03 17:55:46 +0300

    Corrected git ignore + obj files

--- Task02/task02.cpp
+++ Task02/task02.cpp
@@ -1,90 +1,87 @@
-    #include <stdio.h>
-    #include <conio.h>
-    #include <windows.h>
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
 
-    #define THREAD_NUM          3
-    #define BASE_THREAD_NUM     1
-    #define COUNT               10
+#define THREAD_NUM          3
+#define BASE_THREAD_NUM     1
+#define COUNT               10
 
-    CRITICAL_SECTION critsection;
+typedef struct DATA_ {
+    INT threadNum;
+    INT threadIndex;
+    INT value[COUNT];
+} DATA, *PDATA, **PPDATA;
 
-    typedef struct DATA_ {
-        INT threadNum;
-        INT data[COUNT];
-    } DATA, *PDATA;
+typedef struct THREAD_DATA_
+{
+    CRITICAL_SECTION cs;
+    DATA* data[THREAD_NUM];
+} THREAD_DATA, *PTHREAD_DATA;
 
-    DWORD WINAPI PrintValsThread(LPVOID lpParam)
-    {
-        PDATA pVal = (DATA*)lpParam;
-        INT counter = 0;
-        
-        for (USHORT i = 0; i < COUNT; ++i) {
-            if ((counter % 5) == 0)
-                EnterCriticalSection(&critsection);
+DWORD WINAPI PrintValsThread(LPVOID lpParam)
+{
+    THREAD_DATA* thData = (THREAD_DATA*)lpParam;
+    USHORT counter = 0;
 
-            pVal->data[i] = i;
-            printf("Thread %d, i = %d\n", pVal->threadNum, pVal->data[i]);
-            counter++;
+    for (USHORT i = 0; i < COUNT; ++i) {
+        if ((counter % 5) == 0)
+            EnterCriticalSection(&thData->cs);
+
+        thData->data[0]->value[i] = i;
+        printf("Thread %d, i = %d\n", thData->data[0]->threadNum, thData->data[0]->value[i]);
+        counter++;
             
-            if ((counter % 5) == 0 && counter > 0)
-                LeaveCriticalSection(&critsection);
-        }
-        return 0;
+        if ((counter % 5) == 0)
+            LeaveCriticalSection(&thData->cs);
+    }
+    return 0;
+}
+
+INT main(INT argc, WCHAR **argv)
+{
+    HANDLE hThreadArray[THREAD_NUM];
+    DWORD dwthreadNumArray[THREAD_NUM];
+    
+    THREAD_DATA* thData = (THREAD_DATA*)malloc(sizeof(THREAD_DATA));
+    if (thData == NULL) {
+        printf("Malloc failed\n");
+        return -1;
     }
 
-    INT main(INT argc, WCHAR **argv)
-    {
-        PDATA pVal[THREAD_NUM + 1];
-        DWORD dwthreadNumArray[THREAD_NUM];
-        HANDLE hThreadArray[THREAD_NUM];
+    InitializeCriticalSection(&thData->cs);
     
-        InitializeCriticalSection(&critsection);
-        for (INT i = 0; i < THREAD_NUM; ++i) {
-            pVal[i] = (DATA*)malloc(sizeof(DATA));
-            if (pVal[i] == NULL) {
-                printf("pVal[%d] == NULL\n", i);
-                break;
-            }
-            pVal[i]->threadNum = BASE_THREAD_NUM + i;
-       
-            hThreadArray[i] = CreateThread(NULL, 0, PrintValsThread, pVal[i], 0, &dwthreadNumArray[i]);
-            if (hThreadArray[i] == NULL) {
-                printf("hThreadArray[%d] failed, error %d\n", i, GetLastError());
-            break;
-            }
-
+    for (INT i = 0; i < THREAD_NUM; ++i) {
+        thData->data[i] = (DATA*)malloc(sizeof(DATA));
+        if (thData->data == NULL) {
+            printf("Malloc failed\n");
+            return-1;
         }
 
-        DATA mainThreadData;
-        mainThreadData.threadNum = 0;
-        USHORT counter = 0;
+        thData->data[i]->threadNum = BASE_THREAD_NUM + i;
         
-        for (USHORT i = 0; i < COUNT; ++i) {
-            if ((counter % 5) == 0) {
-                EnterCriticalSection(&critsection);
-            }
-            mainThreadData.data[i] = i;
-            printf("Thread %d, i = %d\n", mainThreadData.threadNum, mainThreadData.data[i]);
-            counter++;
-
-            if ((counter % 5) == 0 && counter > 0) {
-                LeaveCriticalSection(&critsection);
-            }
+        hThreadArray[i] = CreateThread(NULL, 0, PrintValsThread, thData, 0, &dwthreadNumArray[i]);
+        if (hThreadArray[i] == NULL) {
+            printf("hThreadArray[%d] failed, error %d\n", i, GetLastError());
+            break;
         }
-
-        INT wait = WaitForMultipleObjects(THREAD_NUM, hThreadArray, TRUE, INFINITE);
-        if (wait == WAIT_FAILED)
-            printf("The function WaitForMultipleObjects() has been failed\n");
-
-        for (INT i = 0; i < THREAD_NUM; ++i) {
-            if (pVal[i] != NULL)
-                free(pVal[i]);
-            CloseHandle(hThreadArray[i]);
-        }
-    
-        DeleteCriticalSection(&critsection);
-        printf("It's over\n");
-
-        _getch();
-        return 0;
     }
+
+    INT wait = WaitForMultipleObjects(THREAD_NUM, hThreadArray, TRUE, INFINITE);
+    if (wait == WAIT_FAILED)
+        printf("The function WaitForMultipleObjects() has been failed\n");
+
+    DeleteCriticalSection(&thData->cs);
+    
+    if (thData != NULL) {
+        for (USHORT i = 0; i < THREAD_NUM; ++i) {
+            if (thData->data[i] != NULL) {
+                free(thData->data[i]);
+            }
+        }
+        free(thData);
+    }
+    printf("It's over\n");
+
+    _getch();
+    return 0;
+}
